d769ad99202f6f229a4d548a713de27cf1fd0c98
Jesse Weed <jesse@jesseweed.com>
2014-05-24 01:41:41 -0700

    lint & optimize server file

--- server.js
+++ server.js
@@ -1,50 +1,57 @@
-// MODULE DEPENDENCIES
-var express = require('express'),
-  fs = require('fs'),
-  env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
-  app = module.exports = express(),
-  server = require('http').createServer(app),
-  io = require('socket.io').listen(server),
-  colors = require('colors'),
-  tesla, port;
+var tesla,
+    express = require('express'), // GET EXPRESS
+    app = module.exports = express(), // DEFINE THE APP
+    server = require('http').createServer(app), // CREATE THE SERVER
+    io = require('socket.io').listen(server); // IN CASE WE WANT SOCKET.IO
+    require('colors'); // PRETTY CONSOLE LOGGING
+    require('fs'); // FILE SYSTEM
+    process.env.NODE_ENV = process.env.NODE_ENV || 'development'; // SET DEFAULT ENVIRONMENT
 
 
-// REQUIRE CONFIG FILES
-require('./config/_settings')(app); // MAIN
-require('./config/environment')(app); // ENVIRONMENT SPECIFIC
-
-// LOAD TESLA LIB
+// LOAD CONFIG & TESLA CLASS
+require('./config/_settings')(app); // MAIN APP SETTINGS
 tesla = require('./lib/tesla')(app);
 
+// WELCOME MESSAGE
 tesla.log(' ');
 tesla.log(' ');
 tesla.log('# # # # # # # # # # # # # # # # # # # # # # # # # # # # #'.rainbow);
-tesla.log('    > > > FIRING UP THE '.white + 'TESLA'.red + ' SERVER. GET HAPPY! < < < '.white);
+tesla.log('       > > > FIRING UP THE SERVER. GET HAPPY! < < < '.white);
 tesla.log('# # # # # # # # # # # # # # # # # # # # # # # # # # # # #'.rainbow);
 tesla.log(' ');
 
-// REQUIRE ADDITIONAL CONFIG FILES
-require('./config/express')(app, tesla); //express settings
-require('./app/routes/_setup')(app, tesla); // routes
+
+// REQUIRED SETTINGS & CONFIG FILES
+require('./config/environment/' + process.env.NODE_ENV)(app); // ENVIRONMENT SPECIFIC SETTINGS
+require('./config/express')(app, tesla); // EXPRESS SETTINGS
+require('./app/routes/_setup')(app, tesla); // DEFAULT ROUTES
+
 
 // START THE APP BY LISTEN ON <PORT>
-port = process.env.PORT || app.config.port;
+server.listen( process.env.PORT || app.config.port, function( err ) {
 
-server.listen(port, function(err) {
+  // IF THERE'S NO ERRORS
+  if ( !err ) {
+    tesla.log(' ');
+    tesla.log('# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #'.green);
+    tesla.log('IT\'S ALIVE! POINT YOUR BROWSER TO: '.white + app.site.url.grey);
+    tesla.log('# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #'.green);
 
-  tesla.log(' ');
-  tesla.log('# # # # # # # # # # # # # # # # # # # # # # # # # # # #'.green);
-  tesla.log('        IT\'S ALIVE!'.white + ' TESLA'.red + ' IS UP AND RUNNING.'.white);
-  tesla.log('   POINT YOUR BROWSER TO: '.grey + app.site.url.white);
-  tesla.log('# # # # # # # # # # # # # # # # # # # # # # # # # # # #'.green);
+  // OH NOES! SOMETHING WENT WRONG!
+  } else {
+    tesla.log(' ');
+    tesla.log('# # # # # # # # # # # # # # # # # # # # # # # # # # # #'.red);
+    tesla.log('ERROR:');
+    tesla.log(err.white);
+    tesla.log('# # # # # # # # # # # # # # # # # # # # # # # # # # # #'.red);
+  }
 
-})
+});
 
 // SOCKETS
 if ( app.config.socket === true ) {
   require('./app/sockets/info.js')(io, app);
 }
-
 
 // EXPOSE APP
 exports = module.exports = app;
