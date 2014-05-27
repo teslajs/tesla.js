ef59f2feccf2f83b83c493f150817d98c71627a8
Jesse Weed <jesse@jesseweed.com>
2014-05-26 18:34:06 -0700

    update settings file

--- config/_settings.js
+++ config/_settings.js
@@ -28,7 +28,7 @@ module.exports = function (app) {
     },
 
     engines : {
-      html : 'jade', // options: (ejs|handlebars|hogan|jade|mustache)
+      html : 'jade', // options: (ejs|hbs|hogan|jade|mustache)
       css : false, // options: (stylus|sass|less) - set false to just use vanilla css
       cssLibrary : false, // options: (axis|bourbon|nib) - set to false for none
     },
