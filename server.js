0f987daca947c05fe804f2bceaf46d0ac41ec373
Jesse Weed <jesse@jesseweed.com>
2014-06-03 01:52:56 -0700

    cleanup & refactor server script

--- server.js
+++ server.js
@@ -6,19 +6,10 @@ var tesla,
     require('fs'); // FILE SYSTEM
     process.env.NODE_ENV = process.env.NODE_ENV || 'development'; // SET DEFAULT ENVIRONMENT
 
-
 // LOAD CONFIG & TESLA CLASS
 require('./config/_settings')(app); // MAIN APP SETTINGS
 tesla = require('./lib/tesla')(app);
-
-// WELCOME MESSAGE
-tesla.log(' ');
-tesla.log(' ');
-tesla.log('# # # # # # # # # # # # # # # # # # # # # # # # # # # # #'.rainbow);
-tesla.log('       > > > FIRING UP THE SERVER. GET HAPPY! < < < '.white);
-tesla.log('# # # # # # # # # # # # # # # # # # # # # # # # # # # # #'.rainbow);
-tesla.log(' ');
-
+tesla.inform(app, 'start'); // WELCOME MESSAGE
 
 // REQUIRED SETTINGS & CONFIG FILES
 require('./config/environment/' + process.env.NODE_ENV)(app); // ENVIRONMENT SPECIFIC SETTINGS
@@ -26,32 +17,22 @@ require('./config/express')(app, tesla); // EXPRESS SETTINGS
 require('./app/routes/default')(app, tesla); // DEFAULT ROUTES
 
 
+// ADD SOCKET.IO
+if ( app.config.socket === true ) {
+  app.io = require('socket.io').listen(server);
+}
+
 // START THE APP BY LISTEN ON <PORT>
 server.listen( process.env.PORT || app.config.port, function( err ) {
 
-  // IF THERE'S NO ERRORS
-  if ( !err ) {
-    tesla.log(' ');
-    tesla.log('# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #'.green);
-    tesla.log('IT\'S ALIVE! POINT YOUR BROWSER TO: '.white + app.site.url.grey);
-    tesla.log('# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #'.green);
-
-  // OH NOES! SOMETHING WENT WRONG!
-  } else {
-    tesla.log(' ');
-    tesla.log('# # # # # # # # # # # # # # # # # # # # # # # # # # # #'.red);
-    tesla.log('ERROR:');
-    tesla.log(err.white);
-    tesla.log('# # # # # # # # # # # # # # # # # # # # # # # # # # # #'.red);
+  if ( !err ) { // IF THERE'S NO ERRORS
+    tesla.inform(app, 'done');
+  } else { // OH NOES! SOMETHING WENT WRONG!
+    tesla.inform(app, 'error', err);
   }
 
 });
 
-// SOCKETS
-if ( app.config.socket === true ) {
-  var io = require('socket.io').listen(server); // IN CASE WE WANT SOCKET.IO
-  require('./app/sockets/info.js')(io, app);
-}
 
 // EXPOSE APP
 exports = module.exports = app;
