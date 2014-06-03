403ea989bea2b98d57ff91cfbbcff0f6ab7ab694
Jesse Weed <jesse@jesseweed.com>
2014-06-03 01:53:19 -0700

    update prod settings for heroku

--- config/environment/production.js
+++ config/environment/production.js
@@ -3,25 +3,12 @@ var os = require('os');
 
 module.exports = function (app) {
 
-  var interfaces = os.networkInterfaces();
-  var addresses = [];
-  for (var k in interfaces) {
-    for (var k2 in interfaces[k]) {
-      var address = interfaces[k][k2];
-      if (address.family === 'IPv4' && !address.internal) {
-        addresses.push(address.address);
-      }
-    }
-  }
-
   var tesla = require('../../lib/tesla')(app);
 
-  if ( typeof addresses[0] === 'undefined' ) addresses[0] = 'localhost';
-
   // global settings
-  app.site.domain = addresses[0];
+  app.site.domain = 'mydomain.com';
   app.site.environment = 'production';
-  app.site.url = app.config.protocol + app.site.domain + ':'  + app.config.port + '/'; // base url
+  app.site.url = app.config.protocol + app.site.domain + '/'; // base url
 
   // directories
   app.site.dir = {
